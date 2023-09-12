c14a76d1c6e59172bf3d173f99c651b99dfd1db3
mubeen090 <144161677+mubeen090@users.noreply.github.com>
2023-09-12 20:06:57 +0530

    first commit

--- components/Home/common/CourseCard/CourseCard.jsx
+++ components/Home/common/CourseCard/CourseCard.jsx
@@ -1,18 +1,29 @@
+jsx
+import React from "react";
 import Image from "next/image";
 import styles from "./CourseCard.module.css";
 import Link from "next/link";
-import lessons from "../../../../public/assets/courseCard/lessons.png";
-import level from "../../../../public/assets/courseCard/level.png";
-import rating4 from "../../../../public/assets/courseCard/rating4.png";
-import students from "../../../../public/assets/courseCard/students.png";
+import lessonsImg from "../../../../public/assets/courseCard/lessons.png";
+import levelImg from "../../../../public/assets/courseCard/level.png";
+import rating4Img from "../../../../public/assets/courseCard/rating4.png";
+import studentsImg from "../../../../public/assets/courseCard/students.png";
+import aiImg from "../../../../public/assets/courseCard/ai.png";
+import mlImg from "../../../../public/assets/courseCard/ml.png";
+import cybersecurityImg from "../../../../public/assets/courseCard/cybersecurity.png";
+import devopsImg from "../../../../public/assets/courseCard/devops.png";
+import oracleImg from "../../../../public/assets/courseCard/oracle.png";
+import awsImg from "../../../../public/assets/courseCard/aws.png";
+import javaImg from "../../../../public/assets/courseCard/java.png";
 
 const CourseCard = ({ slug }) => {
   return (
     <>
+      {/* Course cards on the home screen */}
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/ai.png"
+            src={aiImg}
+            alt="Artificial Intelligence"
             width={340}
             height={240}
             className={styles.course_image}
@@ -21,32 +32,31 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn Artificial Intelligence</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 150+
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects : 4 mini,4 major 
+            <Image src={studentsImg} alt="Students" />
+            Projects: 4 mini, 4 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
-            Baic to advance level
+            <Image src={levelImg} alt="Level" />
+            Basic to advance level
           </div>
         </div>
         <div className={styles.btn_rating}>
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/ml.png"
+            src={mlImg}
+            alt="Machine Learning"
             width={340}
             height={240}
             className={styles.course_image}
@@ -55,15 +65,15 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn Machine Learning</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 120
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects: 3 mini ,4 major 
+            <Image src={studentsImg} alt="Students" />
+            Projects: 3 mini, 4 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
+            <Image src={levelImg} alt="Level" />
             Beginner to advance level
           </div>
         </div>
@@ -71,16 +81,15 @@ const CourseCard = ({ slug }) => {
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/cybersecurity.png"
+            src={cybersecurityImg}
+            alt="Cyber Security"
             width={340}
             height={240}
             className={styles.course_image}
@@ -89,32 +98,31 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn Cyber Security</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 120+
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects: 5 mini ,3 major
+            <Image src={studentsImg} alt="Students" />
+            Projects: 5 mini, 3 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
-            basic to advance level
+            <Image src={levelImg} alt="Level" />
+            Basic to advance level
           </div>
         </div>
         <div className={styles.btn_rating}>
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/devops.png"
+            src={devopsImg}
+            alt="DevOps"
             width={340}
             height={240}
             className={styles.course_image}
@@ -123,15 +131,15 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn DevOps</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 140+
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects : 3mini ,3 major 
+            <Image src={studentsImg} alt="Students" />
+            Projects: 3 mini, 3 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
+            <Image src={levelImg} alt="Level" />
             Basic to Advanced
           </div>
         </div>
@@ -139,16 +147,16 @@ const CourseCard = ({ slug }) => {
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
+      {/* Course cards after exploring */}
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/oracle.png"
+            src={oracleImg}
+            alt="Oracle"
             width={340}
             height={240}
             className={styles.course_image}
@@ -157,32 +165,31 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn Oracle</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 1010+
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects : 4 major projects 
+            <Image src={studentsImg} alt="Students" />
+            Projects: 4 major projects
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
-            basic to advance level
+            <Image src={levelImg} alt="Level" />
+            Basic to advance level
           </div>
         </div>
         <div className={styles.btn_rating}>
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/aws.png"
+            src={awsImg}
+            alt="AWS"
             width={340}
             height={240}
             className={styles.course_image}
@@ -191,15 +198,15 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn AWS</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 180
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects : 4 mini,4 major 
+            <Image src={studentsImg} alt="Students" />
+            Projects: 4 mini, 4 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
+            <Image src={levelImg} alt="Level" />
             Beginner to advance level
           </div>
         </div>
@@ -207,16 +214,15 @@ const CourseCard = ({ slug }) => {
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
 
-      {/* Repeat the above code for other course cards */}
-      
       <div className={styles.course_card}>
         <div className={styles.course_image_container}>
           <Image
-            src="/assets/courseCard/java.png"
+            src={javaImg}
+            alt="Java"
             width={340}
             height={240}
             className={styles.course_image}
@@ -225,23 +231,23 @@ const CourseCard = ({ slug }) => {
         <h1 className={styles.heading}>Learn Java</h1>
         <div className={styles.stats_container}>
           <div className={styles.stats}>
-            <Image src={lessons} />
+            <Image src={lessonsImg} alt="Lessons" />
             Lesson: 190
           </div>
           <div className={styles.stats}>
-            <Image src={students} />
-            projects : 4 mini ,4 major
+            <Image src={studentsImg} alt="Students" />
+            Projects: 4 mini, 4 major
           </div>
           <div className={styles.stats}>
-            <Image src={level} />
-            Basic to advance level 
+            <Image src={levelImg} alt="Level" />
+            Basic to advance level
           </div>
         </div>
         <div className={styles.btn_rating}>
           <Link href={`/course/${slug}`}>
             <button className={styles.btn}>Start Course {">"}</button>
           </Link>
-          <Image src={rating4} />
+          <Image src={rating4Img} alt="Rating" />
         </div>
       </div>
     </>
